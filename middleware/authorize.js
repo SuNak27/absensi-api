ae153ec1572589e15fc6a9271867e41795779503
sunak27 <alfadsabilhaq@gmail.com>
2022-05-24 02:39:00 +0700

    Auth

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,21 +1,19 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken')
-const {
-    JWT_SECRET_KEY
-} = process.env;
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET_KEY } = process.env;
 
 module.exports = async (req, res, next) => {
-    const token = req.get('X-Sambang-Token')
+  const token = req.get("X-Admin-Token");
 
-        jwt.verify(token, JWT_SECRET_KEY, (err, pass) => {
-            if (err){
-                return res.status(401).json({
-                    status: false,
-                    message: err.message
-                })
-            } else if (pass){
-                req.pengurus = pass
-                next()
-            }
-        })
-}
+  jwt.verify(token, JWT_SECRET_KEY, (err, pass) => {
+    if (err) {
+      return res.status(401).json({
+        status: false,
+        message: "Test failed, please login again!",
+      });
+    } else if (pass) {
+      req.admin = pass;
+      next();
+    }
+  });
+};
